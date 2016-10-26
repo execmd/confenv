10910568f02b50e08edc8f7e8df85675af079224
Deniss Perepelicins <deniss.perepelicins@lpb.lv>
2016-10-26 11:24:07 +0300

    Split test to different tests

--- test/main.js
+++ test/main.js
@@ -8,10 +8,21 @@ const config = require('../lib/Confenv.js');
 describe('Confenv', function () {
     it('#read()', function () {
         config.read('test/.env');
+    });
 
+    it('#get() ignore commented', function () {
         assert.equal(config.get('a'),  undefined, 'should be undefined if commented');
+    });
+
+    it('#get() string value', function () {
         assert.equal(config.get('b'), 'value b', 'should be value b');
+    });
+
+    it('#get() object value', function () {
         assert.deepEqual(config.get('c'), { d: 'value d' }, 'should be Object d: value d');
+    });
+
+    it('#get() ignore commented part', function () {
         assert.equal(config.get('e'), 'value e', 'should be value e without comment part');
     });
 
